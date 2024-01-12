e9c9537649ac4af6548fd433c6cd3bf263dd858a
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-01-12 16:06:27 -0300

    add global and theme style

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -10,5 +10,36 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+        
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.colors.neutral.black};
+    border: none;
+    font-family: "Montserrat", "Roboto", sans-serif;
+    
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  button {
+    background: none;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .srOnly {
+    position: absolute;
+    width: 0.1rem;
+    height: 0.1rem;
+    margin: -0.1rem;
+    border-width: 0;
+    overflow: hidden;
+    color: transparent;    
+    clip: rect(0, 0, 0, 0);
   }
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,22 @@
 export const AppTheme = {
-  color: {
-    fire: "#FF0000",
+  colors: {
+    neutral: {
+      black: "#262626",
+      white: "#FDFFFE",
+    },
+    pestel: {
+      pink: "#F5C3F8",
+      blue: "#BFC3FF",
+      purple: "#8E87F9",
+      violet: "#6E67D1",
+    },
+  },
+  size: {
+    vs: 0.8,
+    sm: 1.2,
+    md: 1.6,
+    lg: 2.0,
+    vl: 2.4,
+    el: 3.2,
   },
 };
