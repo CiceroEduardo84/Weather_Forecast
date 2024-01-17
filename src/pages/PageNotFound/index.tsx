cd5d5d5efa3db332336e741dd7cbb1a06e0d3023
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-01-17 18:00:45 -0300

    add style in the page not found

--- src/pages/PageNotFound/index.tsx
+++ src/pages/PageNotFound/index.tsx
@@ -1,11 +1,22 @@
 import { Container } from "./style";
-import page404 from "../../assets/erro404.svg"
+import page404 from "../../assets/erro404.svg";
+import { useNavigate } from "react-router-dom";
 
 export function PageNotFound() {
+  const navigate = useNavigate();
+
+  const returnPage = () => navigate(-1);
+
   return (
     <Container>
-      <h1>PageNotFound</h1>
-      <img src={page404} alt="" />
+      <div className="boxPageNotFound">
+        <img src={page404} />
+        <h1>Page not found</h1>
+      </div>
+      
+      <div className="boxReturnPage">
+        <button onClick={returnPage}>Voltar</button>
+      </div>
     </Container>
   );
 }
--- src/pages/PageNotFound/style.ts
+++ src/pages/PageNotFound/style.ts
@@ -1,5 +1,51 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  color: red;
+export const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 2.8rem;
+  background: linear-gradient(
+    -45deg,
+    ${({ theme }) => theme.colors.pestel.pink},
+    ${({ theme }) => theme.colors.pestel.purple}
+  );
+
+  .boxPageNotFound {
+    width: 100%;
+    padding: 1rem;
+    text-align: center;
+
+    h1 {
+      font-size: 2.4rem;
+      color: ${({ theme }) => theme.colors.neutral.white};
+      text-shadow: -1px 1px 3px #0005;
+    }
+
+    img {
+      width: 30rem;
+      margin: 0 auto;
+      filter: drop-shadow(-1px 1px 3px #0003);
+    }
+  }
+
+  .boxReturnPage {
+    width: 100%;
+    padding: 1rem;
+    display: flex;
+
+    button {
+      width: 12rem;
+      height: 3.4rem;
+      margin: auto;
+      border-radius: 1rem;
+      box-shadow: -1px 1px 3px 0px #0005;
+      text-shadow: -1px 1px 3px #0005;
+      color: ${({ theme }) => theme.colors.neutral.white};
+      background-color: ${({ theme }) => theme.colors.pestel.violet};
+    }
+  }
 `;
