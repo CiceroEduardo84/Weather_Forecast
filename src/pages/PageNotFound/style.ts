22522eb984b030e23348318d144d4dbef7564880
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-01-18 11:21:07 -0300

    add media screen 768px and 1024 px in the page 404

--- src/pages/PageNotFound/style.ts
+++ src/pages/PageNotFound/style.ts
@@ -46,6 +46,70 @@ export const Container = styled.main`
       text-shadow: -1px 1px 3px #0005;
       color: ${({ theme }) => theme.colors.neutral.white};
       background-color: ${({ theme }) => theme.colors.pestel.violet};
+
+      &:hover {
+        filter: brightness(1.05);
+        transform: scale(1.01);
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    gap: 2.8rem;
+
+    .boxPageNotFound {
+      max-width: 50rem;
+      padding: 1.4rem;
+
+      h1 {
+        font-size: 3.2rem;
+        text-shadow: -1px 1px 3px #0006;
+      }
+
+      img {
+        width: 40rem;
+      }
+    }
+
+    .boxReturnPage {
+      padding: 1.4rem;
+      max-width: 50rem;
+
+      button {
+        width: 16rem;
+        height: 3.6rem;
+        box-shadow: -1px 1px 3px 0px #0006;
+        text-shadow: -1px 1px 3px #0006;
+
+        &:hover {
+          transform: scale(1.03);
+        }
+      }
+    }
+  }
+
+  @media (min-width: 1024px) {
+    gap: 3.2rem;
+
+    .boxPageNotFound {
+      padding: 1.8rem;
+
+      h1 {
+        font-size: 3.6rem;
+      }
+
+      img {
+        width: 44rem;
+      }
+    }
+
+    .boxReturnPage {
+      padding: 1.8rem;
+
+      button {
+        width: 16rem;
+        height: 4rem;
+      }
     }
   }
 `;
