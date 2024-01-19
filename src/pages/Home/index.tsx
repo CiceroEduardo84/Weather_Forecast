6af18e258bafcb60e5c8c30cd1c1ba884558e539
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-01-19 00:04:09 -0300

    add style card temperature

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import { useQueryWeatherForecastPage } from "../../hooks/useQueryWeatherForecastPage";
 import { Container } from "./style";
 import refresh from "../../assets/refresh.svg";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { CardStatistics } from "../../components/CardStatistics";
-import locationIcon from "../../assets/Location.png";
+import locationIcon from "../../assets/LocationWhite.png";
 import wind from "../../assets/weatherwind.svg";
 import moisture from "../../assets/weathermoisture.svg";
 import rain from "../../assets/weatherrain.svg";
@@ -24,52 +24,66 @@ export function Home() {
 
       {!isLoading && error && <span className="feedbackError">Error...</span>}
 
-      <section className="boxWeatherForecast">
-        <div className="CradTemperature">
-          <div className="boxLocation">
-            <img src={locationIcon} />
-            <div className="location">
-              <span>{data?.location.name}-ce, Brazil</span>
-              <span>11/01/2024, 13:03</span>
+      {data && (
+        <article>
+          <section className="boxWeatherForecast">
+            <div className="CardTemperature">
+              <div className="boxLocation">
+                <Link to={"/"}>
+                  <img className="imgLocation" src={locationIcon} />
+                </Link>
+
+                <div className="location">
+                  <span>{data?.location.name}-ce, Brazil</span>
+                  <span>11/01/2024, 13:03</span>
+                </div>
+              </div>
+
+              <div className="temperature">
+                <div className="datas">
+                  <strong>
+                    18 <span>°C</span>
+                  </strong>
+                  <span>
+                    22° <span>16°</span>
+                  </span>
+                </div>
+              </div>
+
+              <div className="boxStatistics">
+                <CardStatistics
+                  key={"Vento"}
+                  img={wind}
+                  name="Vento"
+                  data={17}
+                  type={"km/h"}
+                />
+
+                <CardStatistics
+                  key={"Umidade"}
+                  img={moisture}
+                  name="Umidade"
+                  data={31}
+                  type={"%"}
+                />
+
+                <CardStatistics
+                  key={"Chuva"}
+                  img={rain}
+                  name="Chuva"
+                  data={10}
+                  type={"%"}
+                />
+              </div>
             </div>
-          </div>
-          <div className="temperature">
-            <strong>
-              18 <span>°C</span>
-            </strong>
-            <span>22° 16°</span>
-          </div>
-          <div className="boxStatistics">
-            <CardStatistics
-              key={"Vento"}
-              img={wind}
-              name="Vento"
-              data={17}
-              type={"km/h"}
-            />
 
-            <CardStatistics
-              key={"Umidade"}
-              img={moisture}
-              name="Umidade"
-              data={31}
-              type={"%"}
-            />
-
-            <CardStatistics
-              key={"Chuva"}
-              img={rain}
-              name="Chuva"
-              data={10}
-              type={"%"}
-            />
-          </div>
-        </div>
-
-        <div className="boxAirQuality">
-          <h1>{data?.location.name} + aaaa</h1>
-        </div>
-      </section>
+            {/* <div className="boxAirQuality">
+              <h1>{data?.location.name} + aaaa</h1>
+            </div> */}
+          </section>
+          {/* <section className="boxWeekWeather"></section> */}
+        </article>
+      )}
     </Container>
   );
 }
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,7 +1,100 @@
 import styled from "styled-components";
+import clouds from "../../assets/clouds.svg";
 
 export const Container = styled.main`
-  h1 {
-    color: red;
+  width: 100%;
+  height: calc(100vh - 6.4rem);
+  padding: 0 3rem;
+
+  .boxWeatherForecast {
+    width: 26rem;
+    height: 22rem;
+    margin: auto;
+    border-radius: 1rem;
+
+    background: ${({ theme }) => theme.colors.pestel.violet} url(${clouds});
+    background-repeat: no-repeat;
+    background-size: auto;
+    background-repeat: no-repeat;
+    background-position: right bottom;
+    background-attachment: local;
+
+    box-shadow: -1px 1px 3px 0px #0005;
+  }
+
+  .CardTemperature {
+    width: 100%;
+    height: 100%;
+  }
+
+  .boxLocation {
+    width: 100%;
+    padding: 0.4rem;
+    display: flex;
+    justify-content: end;
+    gap: 0.4rem;
+
+    img {
+      width: 2rem;
+      height: 2rem;
+    }
+
+    .location {
+      display: flex;
+      flex-direction: column;
+
+      span {
+        font-size: 1.2rem;
+
+        color: ${({ theme }) => theme.colors.neutral.white};
+      }
+    }
+  }
+
+  .temperature {
+    width: 100%;
+    padding: 0.4rem;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 0.4rem;
+    text-shadow: -1px 1px 3px #0005;
+
+    .datas {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+
+      strong {
+        width: 7.2rem;
+        font-size: 6.2rem;
+        display: flex;
+        color: ${({ theme }) => theme.colors.neutral.white};
+
+        span {
+          width: 7.2rem;
+
+          margin-top: 1.2rem;
+          font-size: 1.8rem;
+
+          color: ${({ theme }) => theme.colors.neutral.white};
+        }
+      }
+
+      span {
+        font-weight: bold;
+        font-size: 1.6rem;
+
+        color: ${({ theme }) => theme.colors.neutral.white};
+
+        span {
+          filter: brightness(0.8);
+        }
+      }
+    }
+  }
+
+  .boxStatistics {
+    width: 100%;
   }
 `;
